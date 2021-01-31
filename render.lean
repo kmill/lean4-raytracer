fc0fa54783901697ab1b4d9f4a0bb9a08d64dc45
Kyle Miller <kmill31415@gmail.com>
2021-01-30 22:51:03 -0800

    metal spheres

--- render.lean
+++ render.lean
@@ -1,4 +1,4 @@
-import render.vec
+import render.vec3
 
 /-- Uniform at random in [0, 1)-/
 def randomFloat {gen} [RandomGen gen] (g : gen) : Float × gen :=
@@ -6,11 +6,21 @@ def randomFloat {gen} [RandomGen gen] (g : gen) : Float × gen :=
   let (lo, hi) := RandomGen.range g
   (Float.ofNat (n - lo) / Float.ofNat (hi - lo + 1), g')
 
-def IO.randFloat : IO Float := do
+def IO.randFloat (lo := 0.0) (hi := 1.0) : IO Float := do
   let gen ← IO.stdGenRef.get
   let (r, gen) := randomFloat gen
   IO.stdGenRef.set gen
-  pure r
+  pure $ lo + (hi - lo) * r
+
+def IO.randVec3 (lo := 0.0) (hi := 1.0) : IO (Vec3 Float) :=
+  return ⟨←IO.randFloat lo hi, ←IO.randFloat lo hi, ←IO.randFloat lo hi⟩
+
+def IO.randVec3InUnitSphere : IO (Vec3 Float) := do
+  for i in [0:100] do -- 8e-29 probability of failure
+    let p ← IO.randVec3 (-1.0) (1.0)
+    if p.lengthSquared >= 1.0 then
+      return p
+  return ⟨1, 0, 0⟩
 
 structure Ray (α : Type _) where
   origin : Vec3 α
@@ -45,6 +55,7 @@ structure HitRecord where
   normal : Vec3 Float
   frontFace : Bool
 
+@[inline]
 def HitRecord.withNormal (p : Vec3 Float) (t : Float) (r : Ray Float) (outwardNormal : Vec3 Float) : HitRecord :=
   let frontFace : Bool := r.dir.dot outwardNormal < 0.0
   let normal : Vec3 Float := if frontFace then outwardNormal else -outwardNormal
@@ -55,14 +66,38 @@ def HitRecord.withNormal (p : Vec3 Float) (t : Float) (r : Ray Float) (outwardNo
     frontFace := frontFace
   }
 
-structure Hittable where
-  hit : Ray Float → Float -> Float -> Option HitRecord
+inductive MaterialResponse
+| absorb
+| scatter (albedo : Color Float) (scattered : Ray Float)
 
-def mkSphere (center : Vec3 Float) (radius : Float) : Hittable where
+structure Material where
+  scatter : Ray Float → HitRecord → IO MaterialResponse
+
+def lambertian (albedo : Color Float) : Material where
+  scatter (r : Ray Float) (hitrec : HitRecord) := do
+    let mut scatterDir := hitrec.normal + (←IO.randVec3InUnitSphere).normalized
+    if scatterDir.nearZero then
+      scatterDir := hitrec.normal
+    let scattered := Ray.mk hitrec.p scatterDir
+    return MaterialResponse.scatter albedo scattered
+
+def metal (albedo : Color Float) : Material where
+  scatter (r : Ray Float) (hitrec : HitRecord) := do
+    let reflected := r.dir.normalized.reflect hitrec.normal
+    let scattered := Ray.mk hitrec.p reflected
+    if scattered.dir.dot hitrec.normal > 0.0 then
+      return MaterialResponse.scatter albedo scattered
+    else
+      return MaterialResponse.absorb
+
+structure Hittable where
+  hit : Ray Float → Float -> Float -> Option (HitRecord × Material)
+
+def mkSphere (center : Vec3 Float) (radius : Float) (mat : Material) : Hittable where
   hit (r : Ray Float) (tmin tmax : Float) := Id.run <| do
     let oc := r.origin - center
     let a := r.dir.lengthSquared
-    let halfb := Vec.dot oc r.dir
+    let halfb := Vec3.dot oc r.dir
     let c := oc.lengthSquared - radius * radius
     let discr := halfb*halfb - a*c
     if discr < 0.0 then
@@ -77,20 +112,25 @@ def mkSphere (center : Vec3 Float) (radius : Float) : Hittable where
     let t := root
     let p := r.at t
     let outwardNormal := (p - center) / radius
-    return some <| HitRecord.withNormal p t r outwardNormal
+    return some (HitRecord.withNormal p t r outwardNormal, mat)
 
-def hitList (hs : List Hittable) (r : Ray Float) (tmin tmax : Float) : Option HitRecord := Id.run <| do
-  let mut hitrec : Option HitRecord := none
+def hitList (hs : List Hittable) (r : Ray Float) (tmin tmax : Float) : Option (HitRecord × Material) := Id.run <| do
+  let mut hitrec : Option (HitRecord × Material) := none
   for obj in hs do
-    let closest := (hitrec.map (HitRecord.t)).getD tmax
+    let closest := (hitrec.map (HitRecord.t ∘ Prod.fst)).getD tmax
     hitrec := obj.hit r tmin closest <|> hitrec
   return hitrec
 
-def rayColor (hs : List Hittable) (r : Ray Float) : Color Float := do
-  match hitList hs r 0 1000000.0 with
-  | some hitrec => do
-    let v := hitrec.normal
-    return 0.5 * Color.mk (v.x + 1.0) (v.y + 1.0) (v.z + 1.0)
+def rayColor (hs : List Hittable) (r : Ray Float) : (depth : Nat) → IO (Color Float)
+| 0 => return ⟨0, 0, 0⟩ -- exceeded ray bounce limit, no more light gathered
+| (depth+1) => do
+  match hitList hs r 0.001 100000000.0 with
+  | some (hitrec, mat) => do
+    match ← mat.scatter r hitrec with
+    | MaterialResponse.absorb =>
+        return Color.mk 0.0 0.0 0.0
+    | MaterialResponse.scatter albedo scattered =>
+        return albedo * (← rayColor hs scattered depth)
   | none => do
     let unit : Vec3 Float := r.dir.normalized
     let t := 0.5 * (unit.y + 1.0)
@@ -103,20 +143,28 @@ def Float.clampToUInt8 (x : Float) : UInt8 :=
   Float.toUInt8 <| Float.min 255 <| Float.max 0 x
 
 def IO.FS.Handle.writeColor (handle : IO.FS.Handle) (c : Color Float) : IO Unit := do
-  let r := Float.clampToUInt8 (256 * c.r)
-  let g := Float.clampToUInt8 (256 * c.g)
-  let b := Float.clampToUInt8 (256 * c.b)
+  let r := Float.clampToUInt8 (256 * c.r.sqrt)
+  let g := Float.clampToUInt8 (256 * c.g.sqrt)
+  let b := Float.clampToUInt8 (256 * c.b.sqrt)
   handle.putStrLn s!"{r} {g} {b}"
 
 def writeTestImage (filename : String) : IO Unit := do
   let width : Nat := 400
   let height : Nat := width * 9 / 16
   let aspectRatio : Float := (Float.ofNat width) / (Float.ofNat height)
-  let samplesPerPixel := 100
+  let samplesPerPixel := 70
+  let maxDepth := 20
+
+  let material_ground := lambertian (Color.mk 0.8 0.8 0.0)
+  let material_center := lambertian (Color.mk 0.7 0.3 0.3)
+  let material_left := metal (Color.mk 0.8 0.8 0.8)
+  let material_right := metal (Color.mk 0.8 0.6 0.2)
 
   let world : List Hittable := [
-    mkSphere (Vec3.mk 0.0 0.0 (-1.0)) 0.5,
-    mkSphere (Vec3.mk 0.0 (-100.5) (-1.0)) 100.0
+    mkSphere (Vec3.mk 0.0 (-100.5) (-1.0)) 100.0 material_ground,
+    mkSphere (Vec3.mk 0.0 0.0 (-1.0)) 0.5 material_center,
+    mkSphere (Vec3.mk (-1.0) 0.0 (-1.0)) 0.5 material_left,
+    mkSphere (Vec3.mk 1.0 0.0 (-1.0)) 0.5 material_right
   ]
 
   let cam := Camera.default aspectRatio
@@ -126,6 +174,7 @@ def writeTestImage (filename : String) : IO Unit := do
     handle.putStrLn "P3"
     handle.putStrLn s!"{width} {height} 255"
     for j' in [0:height] do
+      IO.println s!"line {j'+1} of {height}"
       let j := height - j' - 1
       for i in [0:width] do
         let mut pixelColor := Color.mk 0.0 0.0 0.0
@@ -133,7 +182,7 @@ def writeTestImage (filename : String) : IO Unit := do
           let u := (Float.ofNat i + (← IO.randFloat))/ Float.ofNat width
           let v := (Float.ofNat j + (← IO.randFloat))/ Float.ofNat height
           let ray := cam.getRay u v
-          pixelColor := pixelColor + rayColor world ray
+          pixelColor := pixelColor + (← rayColor world ray maxDepth)
         handle.writeColor (pixelColor / Float.ofNat samplesPerPixel)
 
 def main : List String → IO Unit
---
+++ render/vec3.lean
@@ -0,0 +1,60 @@
+--import render.Algebra
+
+structure Vec3 (α : Type _) :=
+    (x y z : α)
+
+namespace Vec3
+
+@[inline] def map (f : α → β) (v : Vec3 α) : Vec3 β := ⟨f v.x, f v.y, f v.z⟩
+@[inline] def map₂ (f : α → β → γ) (v : Vec3 α) (w : Vec3 β) : Vec3 γ :=
+    ⟨f v.x w.x, f v.y w.y, f v.z w.z⟩
+
+instance [Add α] : Add (Vec3 α) where
+    add := map₂ (. + .)
+instance [Sub α] : Sub (Vec3 α) where
+    sub := map₂ (. - .)
+instance [Mul α] : Mul (Vec3 α) where
+    mul := map₂ (. * .)
+instance [Div α] : Div (Vec3 α) where
+    div := map₂ (. / .)
+instance [Neg α] : Neg (Vec3 α) where
+    neg := map (- .)
+
+-- Scalar multiplication
+instance [Mul α] : HMul α (Vec3 α) (Vec3 α) where
+    hMul c := map (c * .)
+-- Scalar division
+instance [Div α] : HDiv (Vec3 α) α (Vec3 α) where
+    hDiv v c := map (. / c) v
+
+@[inline] def sum [Add α] (v : Vec3 α) : α := v.x + v.y + v.z
+
+@[inline] def lengthSquared [Add α] [Mul α] (v : Vec3 α) : α := (v * v).sum
+
+def length (v : Vec3 Float) : Float := v.lengthSquared.sqrt
+
+def normalized (v : Vec3 Float) : Vec3 Float := v / v.length
+
+@[inline] def dot [Add α] [Mul α] (v w : Vec3 α) : α := (v * w).sum
+
+def cross [Sub α] [Mul α] (v w : Vec3 α) : Vec3 α :=
+    ⟨v.y*w.z - v.z*w.y, v.z*w.x - v.x*w.z, v.x*w.y - v.y*w.x⟩
+
+/-- Reflect v over plane with normal vector n. -/
+def reflect (v n : Vec3 Float) : Vec3 Float := v - 2 * v.dot n * n
+
+def nearZero (v : Vec3 Float) (ε : Float := 1e-8) : Bool :=
+    v.length < ε
+
+end Vec3
+
+abbrev Color (α : Type _) := Vec3 α
+
+namespace Color
+
+def mk (r g b : α) : Color α := ⟨r, g, b⟩
+@[inline] def r (v : Color α) : α := v.x
+@[inline] def g (v : Color α) : α := v.y
+@[inline] def b (v : Color α) : α := v.z
+
+end Color
